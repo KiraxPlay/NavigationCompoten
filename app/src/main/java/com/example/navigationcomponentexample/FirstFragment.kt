a8c2cfc75f6f25767984797b17421f13a178419b
KiraxPlay <sempiternalco2@hotmail.com>
2024-10-27 12:27:40 -0500

    Acciones en los fragments

--- app/src/main/java/com/example/navigationcomponentexample/FirstFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/FirstFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val root : View = inflater.inflate(R.layout.fragment_first, container, false)
+
+        val btnNavi = root.findViewById<Button>(R.id.btnNavi)
+
+        btnNavi.setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+        return root
     }
 
 }
