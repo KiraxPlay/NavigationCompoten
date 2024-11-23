b308fe38dc3977d4326b4a959c69ecfddf6d924b
KiraxPlay <sempiternalco2@hotmail.com>
2024-11-23 10:52:25 -0500

    V7.1

--- app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
@@ -4,12 +4,10 @@ import android.graphics.Color
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.example.navigationcomponentexample.databinding.FragmentSecondBinding
 import java.io.IOException
